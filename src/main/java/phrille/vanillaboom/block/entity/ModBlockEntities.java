e280409b1cde864139f20311c591c9120190b068
Philip Janson <ph8671ja-s@student.lu.se>
2023-04-28 01:14:11 +0200

    Cleanup

--- src/main/java/phrille/vanillaboom/block/entity/ModBlockEntities.java
+++ src/main/java/phrille/vanillaboom/block/entity/ModBlockEntities.java
@@ -8,14 +8,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 import phrille.vanillaboom.VanillaBoom;
 import phrille.vanillaboom.block.ModBlocks;
 
-public class ModBlockEntities
-{
+public class ModBlockEntities {
     private static final DeferredRegister<BlockEntityType<?>> TILE_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, VanillaBoom.MOD_ID);
 
     public static final RegistryObject<BlockEntityType<RainDetectorBlockEntity>> RAIN_DETECTOR = TILE_ENTITY_TYPES.register("rain_detector", () -> BlockEntityType.Builder.of(RainDetectorBlockEntity::new, ModBlocks.RAIN_DETECTOR).build(null));
 
-    public static void init(IEventBus modEventBus)
-    {
+    public static void init(IEventBus modEventBus) {
         TILE_ENTITY_TYPES.register(modEventBus);
     }
 }
