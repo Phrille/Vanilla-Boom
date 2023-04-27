56777a7b9aa86539ada496e377384c7d08934c35
Philip Janson <ph8671ja-s@student.lu.se>
2023-04-28 01:50:17 +0200

    Cleanup

--- src/main/java/phrille/vanillaboom/world/ModConfiguredFeatures.java
+++ src/main/java/phrille/vanillaboom/world/ModConfiguredFeatures.java
@@ -33,7 +33,7 @@ public class ModConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> ORE_BONE_SAND = key("ore_bone_sand");
     public static final ResourceKey<ConfiguredFeature<?, ?>> ORE_WITHER_BONE_SAND = key("ore_wither_bone_sand");
 
-    private static ResourceKey<ConfiguredFeature<?, ?>> key(final String name) {
+    private static ResourceKey<ConfiguredFeature<?, ?>> key(String name) {
         return ResourceKey.create(Registry.CONFIGURED_FEATURE_REGISTRY, new ResourceLocation(VanillaBoom.MOD_ID, name));
     }
 
@@ -41,7 +41,7 @@ public class ModConfiguredFeatures {
     public static class RegistrationHandler {
 
         @SubscribeEvent(priority = EventPriority.LOW)
-        public static void register(final RegistryEvent.Register<Feature<?>> event) {
+        public static void register(RegistryEvent.Register<Feature<?>> event) {
             //OverWorld
             register(ORE_PERIDOTITE, Feature.ORE.configured(Configs.PERIDOTITE_CONFIG).rangeUniform(VerticalAnchor.absolute(0), VerticalAnchor.absolute(79)).squared().count(10));
             //register(DISK_HYDRO_ROCK, Feature.DISK.withConfiguration(new SphereReplaceConfig(ModBlocks.HYDRO_ROCK.getDefaultState(), FeatureSpread.func_242253_a(2, 1), 1, ImmutableList.of(Blocks.STONE.getDefaultState(), Blocks.GRAVEL.getDefaultState()))).withPlacement(Features.Placements.SEAGRASS_DISK_PLACEMENT));
@@ -53,7 +53,7 @@ public class ModConfiguredFeatures {
             register(ORE_WITHER_BONE_SAND, Feature.ORE.configured(Configs.WITHER_BONE_SAND_CONFIG).rangeUniform(VerticalAnchor.absolute(20), VerticalAnchor.absolute(62)).squared().count(8));
         }
 
-        private static void register(final ResourceKey<ConfiguredFeature<?, ?>> key, final ConfiguredFeature<?, ?> configuredFeature) {
+        private static void register(ResourceKey<ConfiguredFeature<?, ?>> key, ConfiguredFeature<?, ?> configuredFeature) {
             Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, key.location(), configuredFeature);
         }
     }
