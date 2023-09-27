f18aede081bfe76ef5666b67de1354c94c8e93af
Philip Janson <ph8671ja-s@student.lu.se>
2023-09-28 00:16:10 +0200

    Fixed config formatting and removed client config

--- src/main/java/phrille/vanillaboom/config/ConfigHandler.java
+++ src/main/java/phrille/vanillaboom/config/ConfigHandler.java
@@ -10,16 +10,8 @@ import phrille.vanillaboom.VanillaBoom;
 
 @EventBusSubscriber(modid = VanillaBoom.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public final class ConfigHandler {
-    public static final Client CLIENT;
-    public static final ForgeConfigSpec CLIENT_SPEC;
     public static final Common COMMON;
     public static final ForgeConfigSpec COMMON_SPEC;
-
-    static {
-        Pair<Client, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Client::new);
-        CLIENT_SPEC = specPair.getRight();
-        CLIENT = specPair.getLeft();
-    }
 
     static {
         Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
@@ -31,24 +23,9 @@ public final class ConfigHandler {
     public static void onModConfigEvent(ModConfigEvent event) {
         ModConfig config = event.getConfig();
 
-        if (config.getSpec() == CLIENT_SPEC) {
-            VanillaBoomConfig.bakeClient(config);
-            VanillaBoom.LOGGER.debug("Baked client config");
-        } else if (config.getSpec() == COMMON_SPEC) {
+        if (config.getSpec() == COMMON_SPEC) {
             VanillaBoomConfig.bakeCommon(config);
             VanillaBoom.LOGGER.debug("Baked common config");
-        }
-    }
-
-    public static class Client {
-        public final ForgeConfigSpec.BooleanValue addFoodTooltips;
-
-        public Client(ForgeConfigSpec.Builder builder) {
-            builder.push("general");
-
-            addFoodTooltips = builder.comment(" Set this to false to disable food icons being added to tooltips").define("add_food_tool_tips", true);
-
-            builder.pop();
         }
     }
 
@@ -72,25 +49,21 @@ public final class ConfigHandler {
 
         public Common(ForgeConfigSpec.Builder builder) {
             builder.push("general");
-
-            growNetherWarts = builder.comment(" Set this to false to disable Blaze Powder or Wither Bone Meal being used to grow Nether Wart.").define("grow_nether_warts", true);
+            growNetherWarts = builder.comment("\n Set this to false to disable Blaze Powder or Wither Bone Meal being used to grow Nether Wart.").define("grow_nether_warts", true);
             growWitherRoses = builder.comment("\n Set this to false to disable Wither Bone Meal being used to grow Roses into Wither Roses.").define("grow_wither_roses", true);
             placeSlimeBallPistons = builder.comment("\n Set this to false to disable placing Slime Balls on Sticky Pistons when right clicking with a Slime Ball.").define("place_slime_ball_pistons", true);
             removeSlimeBallPistons = builder.comment("\n Set this to false to disable removing Slime Balls from Sticky Pistons when right clicking and crouching with a Shovel.").define("remove_slime_ball_pistons", true);
             fillWaterBottleHydroRock = builder.comment("\n Set this to false to disable Glass Bottles being able to be filled up when right clicking a Hydro Rock block.").define("fill_water_bottle_hydro_rock", true);
-
             builder.pop();
+
             builder.comment(" Warning: \n Changing world gen config options will require \n the world to be reloaded to take action. \n For servers this means a server restart.");
             builder.push("worldgen");
-
-            peridotiteGenEnabled = builder.comment(" Set this to false to stop Peridotite being generated in the Overworld.").define("peridotite_gen", true);
+            peridotiteGenEnabled = builder.comment("\n Set this to false to stop Peridotite being generated in the Overworld.").define("peridotite_gen", true);
             hydroRockGenEnabled = builder.comment("\n Set this to false to stop Hydro Rock being generated in the Overworld.").define("hydro_rock_gen", true);
             roseGenEnabled = builder.comment("\n Set this to false to stop Roses being generated in the Overworld.").define("rose_gen", true);
-
             infernalRockGenEnabled = builder.comment("\n Set this to false to stop Infernal Rock being generated in the Nether.").define("infernal_rock_gen", true);
             boneSandGenEnabled = builder.comment("\n Set this to false to stop Bone Sand being generated in the Nether.").define("bone_sand_gen", true);
             witherBoneSandGenEnabled = builder.comment("\n Set this to false to stop Wither Bone Sand being generated in the Nether.").define("wither_bone_sand_gen", true);
-
             builder.pop();
         }
     }
