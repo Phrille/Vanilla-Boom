e76372fa793d644e81341a786e69b06427f1d09a
Philip Janson <ph8671ja-s@student.lu.se>
2023-10-02 20:00:07 +0200

    Update GLMs to 1.19

--- src/main/java/phrille/vanillaboom/loot/FishingLootModifier.java
+++ src/main/java/phrille/vanillaboom/loot/FishingLootModifier.java
@@ -1,42 +1,51 @@
 package phrille.vanillaboom.loot;
 
-import com.google.common.collect.Lists;
-import com.google.gson.JsonObject;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.GsonHelper;
+import com.google.common.base.Suppliers;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootTableReference;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Set;
+import java.util.function.Supplier;
 
 public class FishingLootModifier extends LootModifier {
-    private static final Field LOOT_FIELD = ObfuscationReflectionHelper.findField(LootContext.class, "f_78911_");
-    protected final LootTableReference lootTable;
-    protected final float replaceChance;
+    public static final Supplier<Codec<FishingLootModifier>> CODEC = Suppliers.memoize(() ->
+            RecordCodecBuilder.create(inst -> codecStart(inst).and(
+                    inst.group(
+                            Codec.STRING.fieldOf("table").xmap(LootTableHandler::getLootTableReference, LootTableHandler::getString).forGetter(m -> m.lootTable),
+                            Codec.FLOAT.fieldOf("chance").forGetter(m -> m.chance)
+                    )).apply(inst, FishingLootModifier::new)
+            )
+    );
 
-    public FishingLootModifier(LootItemCondition[] conditions, LootTableReference lootTable, float replaceChance) {
+    private static final Field LOOT_FIELD = ObfuscationReflectionHelper.findField(LootContext.class, "f_78911_");
+    private final LootTableReference lootTable;
+    private final float chance;
+
+    public FishingLootModifier(LootItemCondition[] conditions, LootTableReference lootTable, float chance) {
         super(conditions);
         this.lootTable = lootTable;
-        this.replaceChance = replaceChance;
+        this.chance = chance;
     }
 
     @Nonnull
     @Override
-    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
         try {
             Set<LootTable> set = (Set<LootTable>) LOOT_FIELD.get(context);
 
-            if (set.isEmpty() && context.getRandom().nextFloat() <= replaceChance) {
-                List<ItemStack> loot = Lists.newArrayList();
+            if (set.isEmpty() && context.getRandom().nextFloat() <= chance) {
+                ObjectArrayList<ItemStack> loot = ObjectArrayList.of();
                 lootTable.createItemStack(loot::add, context);
 
                 return loot;
@@ -48,24 +57,8 @@ public class FishingLootModifier extends LootModifier {
         }
     }
 
-    public static class Serializer extends GlobalLootModifierSerializer<FishingLootModifier> {
-
-        @Override
-        public FishingLootModifier read(ResourceLocation location, JsonObject object, LootItemCondition[] lootConditions) {
-            String resLoc = GsonHelper.getAsString(object, "table");
-            LootTableReference table = (LootTableReference) LootTableReference.lootTableReference(new ResourceLocation(resLoc)).build();
-            float chance = GsonHelper.getAsFloat(object, "chance");
-
-            return new FishingLootModifier(lootConditions, table, chance);
-        }
-
-        @Override
-        public JsonObject write(FishingLootModifier instance) {
-            JsonObject json = makeConditions(instance.conditions);
-            json.addProperty("table", instance.lootTable.name.toString());
-            json.addProperty("chance", instance.replaceChance);
-
-            return json;
-        }
+    @Override
+    public Codec<? extends IGlobalLootModifier> codec() {
+        return CODEC.get();
     }
 }
--- src/main/java/phrille/vanillaboom/loot/LootTableHandler.java
+++ src/main/java/phrille/vanillaboom/loot/LootTableHandler.java
@@ -1,18 +1,25 @@
 package phrille.vanillaboom.loot;
 
-import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import com.mojang.serialization.Codec;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.entries.LootTableReference;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import phrille.vanillaboom.VanillaBoom;
 
 public class LootTableHandler {
-    public static final DeferredRegister<GlobalLootModifierSerializer<?>> GLMS = DeferredRegister.create(ForgeRegistries.Keys.LOOT_MODIFIER_SERIALIZERS, VanillaBoom.MOD_ID);
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> GLMS = DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, VanillaBoom.MOD_ID);
 
-    public static final RegistryObject<DropLootModifier.Serializer> WITHER_SKELETON = GLMS.register("wither_skeleton", DropLootModifier.Serializer::new);
-    public static final RegistryObject<DropLootModifier.Serializer> SILVERFISH = GLMS.register("silverfish", DropLootModifier.Serializer::new);
-    public static final RegistryObject<DropLootModifier.Serializer> DROWNED = GLMS.register("drowned", DropLootModifier.Serializer::new);
-    public static final RegistryObject<DropLootModifier.Serializer> POLAR_BEAR = GLMS.register("polar_bear", DropLootModifier.Serializer::new);
-    public static final RegistryObject<DropLootModifier.Serializer> SPRUCE_LEAVES = GLMS.register("spruce_leaves", DropLootModifier.Serializer::new);
-    public static final RegistryObject<FishingLootModifier.Serializer> FISHING = GLMS.register("fishing", FishingLootModifier.Serializer::new);
+    public static final RegistryObject<Codec<DropLootModifier>> DROP_LOOT_MODIFIER = GLMS.register("drop_loot_modifier", DropLootModifier.CODEC);
+    public static final RegistryObject<Codec<FishingLootModifier>> FISHING_MODIFIER = GLMS.register("fishing_modifier", FishingLootModifier.CODEC);
+
+    public static LootTableReference getLootTableReference(String resLoc) {
+        return (LootTableReference) LootTableReference.lootTableReference(new ResourceLocation(resLoc)).build();
+    }
+
+    public static String getString(LootTableReference lootTable) {
+        return lootTable.name.toString();
+    }
 }
