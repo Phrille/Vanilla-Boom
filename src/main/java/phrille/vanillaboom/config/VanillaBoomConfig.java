676a0565adbed4368ed973e9ec019711319d2ff1
Philip Janson <ph8671ja-s@student.lu.se>
2024-10-10 14:19:11 +0200

    Finalize for 1.19.2

--- src/main/java/phrille/vanillaboom/config/VanillaBoomConfig.java
+++ src/main/java/phrille/vanillaboom/config/VanillaBoomConfig.java
@@ -10,30 +10,11 @@ public class VanillaBoomConfig {
     public static boolean removeSlimeBallPistons;
     public static boolean fillWaterBottleHydroRock;
 
-    //Worldgen Overworld
-    public static boolean peridotiteGenEnabled;
-    public static boolean hydroRockGenEnabled;
-    public static boolean roseGenEnabled;
-
-    //Worldgen Nether
-    public static boolean infernalRockGenEnabled;
-    public static boolean boneSandGenEnabled;
-    public static boolean witherBoneSandGenEnabled;
-
-
     public static void bakeCommon(ModConfig config) {
         growNetherWarts = ConfigHandler.COMMON.growNetherWarts.get();
         growWitherRoses = ConfigHandler.COMMON.growWitherRoses.get();
         placeSlimeBallPistons = ConfigHandler.COMMON.placeSlimeBallPistons.get();
         removeSlimeBallPistons = ConfigHandler.COMMON.removeSlimeBallPistons.get();
         fillWaterBottleHydroRock = ConfigHandler.COMMON.fillWaterBottleHydroRock.get();
-
-        peridotiteGenEnabled = ConfigHandler.COMMON.peridotiteGenEnabled.get();
-        hydroRockGenEnabled = ConfigHandler.COMMON.hydroRockGenEnabled.get();
-        roseGenEnabled = ConfigHandler.COMMON.roseGenEnabled.get();
-
-        infernalRockGenEnabled = ConfigHandler.COMMON.infernalRockGenEnabled.get();
-        boneSandGenEnabled = ConfigHandler.COMMON.boneSandGenEnabled.get();
-        witherBoneSandGenEnabled = ConfigHandler.COMMON.witherBoneSandGenEnabled.get();
     }
 }
