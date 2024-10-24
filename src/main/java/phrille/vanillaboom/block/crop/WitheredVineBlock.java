41b6072d9c180f41e92b9093f3f3dde9a9c56e4a
Philip Janson <ph8671ja-s@student.lu.se>
2024-10-24 17:24:20 +0200

    Fix withered vine not updating correctly

--- src/main/java/phrille/vanillaboom/block/crop/WitheredVineBlock.java
+++ src/main/java/phrille/vanillaboom/block/crop/WitheredVineBlock.java
@@ -1,16 +1,22 @@
 package phrille.vanillaboom.block.crop;
 
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.VineBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+@MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 public class WitheredVineBlock extends VineBlock {
     public WitheredVineBlock() {
@@ -20,5 +26,45 @@ public class WitheredVineBlock extends VineBlock {
     @Override
     public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource rand) {
         //No-op implementation
+    }
+
+    @Override
+    public boolean canSupportAtFace(BlockGetter getter, BlockPos pos, Direction direction) {
+        if (direction == Direction.DOWN) {
+            return false;
+        } else {
+            if (VineBlock.isAcceptableNeighbour(getter, pos.relative(direction), direction)) {
+                return true;
+            } else if (direction.getAxis() == Direction.Axis.Y) {
+                return false;
+            } else {
+                BlockState aboveState = getter.getBlockState(pos.above());
+                return aboveState.is(Blocks.VINE) && aboveState.getValue(PROPERTY_BY_DIRECTION.get(direction));
+            }
+        }
+    }
+
+    @Override
+    public BlockState getUpdatedState(BlockState state, BlockGetter getter, BlockPos pos) {
+        BlockPos above = pos.above();
+        if (state.getValue(UP)) {
+            state = state.setValue(UP, VineBlock.isAcceptableNeighbour(getter, above, Direction.DOWN));
+        }
+
+        BlockState aboveState = null;
+        for (Direction direction : Direction.Plane.HORIZONTAL) {
+            BooleanProperty property = getPropertyForFace(direction);
+            if (state.getValue(property)) {
+                boolean canSupportFace = canSupportAtFace(getter, pos, direction);
+                if (!canSupportFace) {
+                    if (aboveState == null) {
+                        aboveState = getter.getBlockState(above);
+                    }
+                    canSupportFace = aboveState.is(Blocks.VINE) && aboveState.getValue(property);
+                }
+                state = state.setValue(property, canSupportFace);
+            }
+        }
+        return state;
     }
 }
