bee4ebb3d4be43f521ce8503e14cf40b6f9e1dad
Philip Janson <ph8671ja-s@student.lu.se>
2025-04-01 07:14:35 +0200

    Copyright headers

--- src/main/java/phrille/vanillaboom/util/CommonTags.java
+++ src/main/java/phrille/vanillaboom/util/CommonTags.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2025 Phrille
+ *
+ * This file is part of the Vanilla Boom Mod.
+ * Unauthorized distribution or modification is prohibited.
+ * See LICENSE for details.
+ */
+
 package phrille.vanillaboom.util;
 
 import net.minecraft.resources.ResourceLocation;
